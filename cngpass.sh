65b9e13fc3c6486b8a0b911235a74eda2404cb24
Aguinaldo Bezerra <aguinaldoabbj@users.noreply.github.com>
2019-11-04 14:07:55 -0300

    Update cngpass.sh

--- cngpass.sh
+++ cngpass.sh
@@ -3,7 +3,7 @@
 sleep 5
 
 # changing default password on lauching container
-curl -H 'Content-Type: application/json' -XPOST -d '{"password":$NEO4J_PASS}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+curl -H 'Content-Type: application/json' -XPOST -d '{"password":"'"$NEO4J_PASS"'"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
 
 
 
